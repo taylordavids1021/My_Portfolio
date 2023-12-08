a80c554355f09912c1e45c2d3201e932c284914d
Taylor <taylordavids997@gmail.com>
2023-12-08 14:04:31 +0200

    instal vite to build my react porfolio

---
+++ My_Portfolio/src/App.jsx
@@ -0,0 +1,16 @@
+// import { useState } from 'react'
+import Home from './components/Home'
+import AboutMe from './components/AboutMe'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Home />
+      <AboutMe />
+    </>
+  )
+}
+
+export default App
