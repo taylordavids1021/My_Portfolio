c281dc6f2e01802b839eb32b6775cb039823962f
Taylor <taylordavids997@gmail.com>
2024-01-19 11:33:21 +0200

    navbar structure

--- My_Portfolio/src/components/Navbar.jsx
+++ My_Portfolio/src/components/Navbar.jsx
@@ -1,7 +1,22 @@
+import html from '../assets/html.png'
+import css from '../assets/css.png'
+import js from '../assets/js.png'
+import atom from '../assets/atom.png'
+
 function Navbar() {
     return (
-        <div>
-
+        <div className="navbar--container">
+            <div className="div">
+                <img src={html}/>
+                <img src={css}/>
+                <img src={js}/>
+                <img src={atom}/>
+            </div>
+            <ul className="navbar--list">
+                <li className="navbar--list--items">About Me</li>
+                <li className="navbar--list--items">Projects</li>
+                <li className="navbar--list--items">Contact</li>
+            </ul>
         </div>
     )
 }
