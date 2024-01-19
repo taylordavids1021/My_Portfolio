378c12281af410bc0dbbfb4b50a3c938ceb4b8ae
Taylor <taylordavids997@gmail.com>
2024-01-19 11:33:33 +0200

    home structure

--- My_Portfolio/src/components/Home.jsx
+++ My_Portfolio/src/components/Home.jsx
@@ -1,8 +1,17 @@
+import AboutMe from './AboutMe'
+import Navbar from './Navbar'
+
 function Home() {
     return (
-        <div>
-            <h1>Testing</h1>
-        </div>
+        <section>
+            <Navbar />
+            <div className='welcome'>
+                <h1>Welcome to Taylor's Portfolio!</h1>
+                <h3>Passionate Software Developer</h3>
+                <p>Hello and welcome! I'm Taylor Abrahams, a dedicated and enthusiastic entry-level software developer with a passion for creating efficient and user-friendly applications. I'm excited to share my journey, projects, and skills with you</p>
+            </div>
+            <AboutMe />
+        </section>
     )
 }
 
