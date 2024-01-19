3cba9b4755508c7b04cacabfcd763b92de622d7a
Taylor <taylordavids997@gmail.com>
2024-01-19 11:32:34 +0200

    about me content

--- My_Portfolio/src/components/AboutMe.jsx
+++ My_Portfolio/src/components/AboutMe.jsx
@@ -1,9 +1,9 @@
 function AboutMe() {
     return (
-        <div>
+        <div className="about--me">
             <h1>About Me</h1>
-            <p>I create fantasy worlds and brand personalities with my trusty stylus and boundless imagination. I Can't wait to collaborate with you!</p> 
-            <p>I am Enthusiastic and detail-oriented entry-level front-end developer with a solid foundation in HTML, CSS, and JavaScript. Adept at turning design concepts into responsive and user-friendly websites. Eager to contribute my technical skills and passion for coding to a dynamic development team. Possess a strong commitment to staying current with emerging technologies and a demonstrated ability to quickly learn and apply new concepts. Excited to leverage my education and hands-on projects to contribute effectively to innovative web development projects.</p>
+            <p>I create fantasy worlds and brand personalities with my trusty stylus and boundless imagination. <br></br>I Cant wait to collaborate with you!</p> 
+            <p>I am Enthusiastic and detail-oriented entry-level front-end developer with a solid foundation in HTML, CSS, and JavaScript. Adept at turning design concepts into responsive and user-friendly websites.<br></br>Eager to contribute my technical skills and passion for coding to a dynamic development team. Possess a strong commitment to staying current with emerging technologies and a demonstrated ability to quickly learn and apply new concepts. Excited to leverage my education and hands-on projects to contribute effectively to innovative web development projects.</p>
         </div>
     )
 }
